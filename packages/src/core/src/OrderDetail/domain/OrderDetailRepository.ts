cfe25c13f00fbac4b957a7ff42dea43c8e41f8db
Pablo Guallichico <pablo.guallichico@outlook.com>
2024-05-31 16:07:03 -0500

    feat(deliveryDetail): add deliveryDetail entity

--- packages/src/core/src/OrderDetail/domain/OrderDetailRepository.ts
+++ packages/src/core/src/OrderDetail/domain/OrderDetailRepository.ts
@@ -1,7 +1,7 @@
 import { Criteria } from "../../shared/domain/Criteria";
 import { Filter } from "../../shared/domain/Criteria/Filter";
 import { Nullable } from "../../shared/domain/Nullable";
-import { OrderDetail, OrderDetailId } from "./Client";
+import { OrderDetail, OrderDetailId } from "./OrderDetail";
 
 export interface OrderDetailRepository {
   save(orderDetail: OrderDetail): Promise<void>;
