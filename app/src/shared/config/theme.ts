99e09b4419d43ee708f31cd97d9bec3dd042f9c9
Pablo Guallichico <pablo.guallichico@outlook.com>
2024-08-20 21:31:04 -0500

    fix(theme): fix theme

--- app/src/shared/config/theme.ts
+++ app/src/shared/config/theme.ts
@@ -1,33 +1,33 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-    palette: {
-      mode: "light",
-      background: {
-        default: '#FFFFFF',
-        paper: '#F0F0F0',
-      },
-      text: {
-        primary: '#333333',
-        secondary: '#555555',
-      },
-      primary: {
-        main: '#007BFF',
-      },
-      secondary: {
-        main: '#6C757D',
-      },
-      success: {
-        main: '#28A745',
-      },
-      warning: {
-        main: '#FFC107',
-      },
-      error: {
-        main: '#DC3545',
-      },
-      info: {
-        main: '#17A2B8',
-      },
+  palette: {
+    mode: "light",
+    background: {
+      default: '#FFFFFF',
+      paper: '#F0F0F0',
+    },
+    text: {
+      primary: '#333333',
+      secondary: '#555555',
+    },
+    primary: {
+      main: '#007BFF',
+    },
+    secondary: {
+      main: '#6C757D',
+    },
+    success: {
+      main: '#28A745',
+    },
+    warning: {
+      main: '#FFC107',
+    },
+    error: {
+      main: '#DC3545',
+    },
+    info: {
+      main: '#17A2B8',
+    },
   }
 });
