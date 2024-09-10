0c2ab08e8007aa45f92942e4d682e159ef7182dd
Pablo Guallichico <pablo.guallichico@outlook.com>
2024-09-09 20:40:01 -0500

    fix(api): fix error types in mutation

--- packages/src/api/src/product/mutations/deleteProduct.mutation.ts
+++ packages/src/api/src/product/mutations/deleteProduct.mutation.ts
@@ -4,15 +4,24 @@ import { injector } from "../product.injector";
 builder.mutationField("deleteProduct", (t) =>
   t.field({
     type: "response",
+    errors: {},
     args: {
       productId: t.arg.string({ required: true }),
     },
     resolve: async (_, { productId }) => {
-      await injector.productDeleter.run(productId);
-      return {
-        error: false,
-        success: true,
-      };
+      try {
+        await injector.productDeleter.run(productId);
+        return {
+          error: false,
+          success: true,
+        };
+      } catch (e) {
+        return {
+          error: true,
+          success: false,
+        };
+      }
+
     }
   })
 );
